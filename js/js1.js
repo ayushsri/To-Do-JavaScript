6adee16367eb9cd731f25cbe866565a125b86ebd
Ayush-Srivastava-northeastern <srivastava.ayu@northeastern.edu>
2021-03-03 16:14:08 -0500

    done and dusted

--- js/js1.js
+++ js/js1.js
@@ -1,28 +1,34 @@
 //Getting the JSON data
 const xhr=new XMLHttpRequest();
+//Getting JSON data using todo.json
 xhr.open('GET','Data/todo.json');
+//using xhr object
 xhr.onload=(response)=>{
     if(response.currentTarget.status===200)
-    {
+    {//parsing json
         const names=JSON.parse(response.currentTarget.responseText);
         createList(names);
     }
 
 }
-xhr.send();
 
+xhr.send();
+//creating function to render the json
 const createList=(names)=> {
     const container = document.getElementsByClassName("hmm").item(0);
     //const list =document.createElement("ul");
     let listItem;
     let list3;
-
+//creating an li and div element and rendering loop
     names.forEach(name => {
         let stat="notComplete";
         listItem = document.createElement('li');
+        //creating li element
         listItem.id = 'item-${name.title}';
+        //seting the id
         listItem.setAttribute('data-index', name.title)
         listItem.textContent = name.title;
+        //getting the list item
         listItem.classList.add("list-Item");
         let button1 = document.createElement("button1");
         let button2 = document.createElement("button2");
@@ -30,6 +36,8 @@ const createList=(names)=> {
         button2.innerHTML = "complete";
         button2.className = "button2";
         button1.className = "button1";
+
+        //two buttons to get input
         list3=document.createElement("div");
         list3.id = name.title;
         list3.textContent ="Description: " +name.description+" Status: "+stat+" Due Date :"+ name.dueDate+" Time: "+name.dueTime;
@@ -37,11 +45,13 @@ const createList=(names)=> {
         listItem.appendChild(button1);
         listItem.appendChild(button2);
 
-
-        var span = document.createElement("SPAN");
-        var txt = document.createTextNode("\u00D7");
+     //using span for the input
+        let span = document.createElement("SPAN");
+        let txt = document.createTextNode("\u00D7");
         span.className = "close";
+        let close = document.getElementsByClassName("close");
         span.appendChild(txt);
+        // append the child to parent element
         listItem.appendChild(span);
         button2.addEventListener("click", function () {
 
@@ -56,7 +66,7 @@ const createList=(names)=> {
           /*  this.parentElement.textContent= name.title+"\r\n"+name.dueDate;
           listItem.appendChild(button2);
             listItem.appendChild(span);*/
-            var div = document.getElementById(name.title);
+            let div = document.getElementById(name.title);
             if (div.style.display !== "none") {
                 div.style.display = "none";
             }
@@ -66,91 +76,56 @@ const createList=(names)=> {
 
         })
 
-        container.appendChild(listItem);
+    //appendng all data
         container.appendChild(list3);
+        container.appendChild(listItem);
+        //close the div
         for (i = 0; i < close.length; i++) {
             close[i].onclick = function () {
-                var div = this.parentElement;
+                let div = this.parentElement;
                 div.style.display = "none";
             }
         }
-
-       // button1.addEventListener("click", function () {
-
-
-
-                //list2.setAttribute('data-index2', "ayush");
-
-          //  }, false);
-
-
-
 
 
     })
 
 }
 
-var myNodelist = document.getElementsByClassName("list-Item");
-var i;
-console.log(myNodelist);
-for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
-
-
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-}
-
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-        //ev.target.classList.toggle('checked');
-        const x = document.getElementById("myInput");
-        if (x.id === "myInput") {
-            x.innerHTML = "Swapped text!";
-        } else {
-            x.innerHTML = "Hello";
-        }
-    }
-
-}, false);
-
 // Create a new list item when clicking on the "Add" button
-function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
+function fun123() {
+  //make second container
+    const container1 = document.getElementsByClassName("hmm").item(0);
+    //set the status as not complete
+    let stat1="Not Complete";
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("myInput").value;
+    //get the description value
+    let discriptionValue = document.getElementById("description").value;
+    let dateValue= document.getElementById("dueDate").value;
+    let timeValue= document.getElementById("dueTime").value;
+    let t = document.createTextNode(inputValue);
     li.appendChild(t);
+    //if string is null
     if (inputValue === '') {
         alert("You must write something!");
+        return;
     } else {
         document.getElementById("myUL").appendChild(li);
     }
     document.getElementById("myInput").value = "";
 
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
+    //append the child
+    let close = document.getElementsByClassName("close");
 
-    for (i = 0; i < close.length; i++) {
+    for (let i = 0; i < close.length; i++) {
         close[i].onclick = function() {
-            var div = this.parentElement;
+            let div = this.parentElement;
             div.style.display = "none";
         }
         let button1 = document.createElement("button1");
@@ -160,46 +135,70 @@ function newElement() {
         button2.className = "button2";
         button1.className = "button1";
 
+
+        let list4;
+        list4=document.createElement("div");
+        list4.id = inputValue ;
+        list4.textContent ="Description: " +discriptionValue+" Status: "+"Not complete"+" Due Date :"+ dateValue+" Time: "+timeValue;
+        list4.style.display="none";
+
+
+
         button2.addEventListener("click", function () {
 
             this.parentElement.style.backgroundColor = "yellow";
+            stat1="complete";
+            list4.textContent ="Description: " +discriptionValue+" Status: "+stat1+" Due Date :"+dateValue+" Time: "+timeValue;
 
         })
 
 
+
         button1.addEventListener("click", function () {
-            this.parentElement.textContent= name.title+"\r\n"+name.dueDate;
-            li.appendChild(button2);
-            li.appendChild(span);
+            /*  this.parentElement.textContent= name.title+"\r\n"+name.dueDate;
+            listItem.appendChild(button2);
+              listItem.appendChild(span);*/
+            let div = document.getElementById(inputValue);
+            if (div.style.display !== "none") {
+                div.style.display = "none";
+            }
+            else {
+                div.style.display = "block";
+            }
 
         })
 
         li.appendChild(button1);
         li.appendChild(button2);
+        container1.appendChild(li);
+        container1.appendChild(list4);
+
 
     }
 }
+//validating the date
+let today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth()+1;
+let yyyy = today.getFullYear();
 
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth()+1; //January is 0 so need to add 1 to make it 1!
-var yyyy = today.getFullYear();
+/** jan starts with 0**/
 if(dd<10){
     dd='0'+dd
 }
 if(mm<10){
     mm='0'+mm
 }
-
+//today
 today = yyyy+'-'+mm+'-'+dd;
 document.getElementById("dueDate").setAttribute("min", today);
+//today value
+let today1 = new Date();
+let time = today1.getHours() + ":" + today1.getMinutes() + ":" + today1.getSeconds();
 
-var today1 = new Date();
-var time = today1.getHours() + ":" + today1.getMinutes() + ":" + today1.getSeconds();
-
-var date1=document.getElementById("dueDate").value;
+let date1=document.getElementById("dueDate").value;
 console.log(date1);
-
+//today
 if(date1==today)
 {
     document.getElementById("dueTime").min = time;
